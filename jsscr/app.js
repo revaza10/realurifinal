875b8110eabf8677d5267361f5b0cadc7843e366
revaza10 <132154536+revaza10@users.noreply.github.com>
2024-06-14 02:36:03 +0400

    d
    d

---
+++ jsscr/app.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var menu = document.querySelector('.hidden-menu');
+    menu.style.display === 'block' ? menu.style.display = 'none' : menu.style.display = 'block';
+  
+    var navLinks = document.querySelectorAll('.navbar a');
+    navLinks.forEach(link => {
+      link.style.display === 'block' ? link.style.display = 'none' : link.style.display = 'block';
+    });
+    
+  }
+  
