a31075d748ece165607c48a9b36a68e3c3c03933
revaza10 <132154536+revaza10@users.noreply.github.com>
2024-06-14 23:53:06 +0400

    სდა
    სდა

--- jsscr/app.js
+++ jsscr/app.js
@@ -2,10 +2,10 @@ function toggleMenu() {
     var menu = document.querySelector('.hidden-menu');
     menu.style.display === 'block' ? menu.style.display = 'none' : menu.style.display = 'block';
   
-    var navLinks = document.querySelectorAll('.navbar a');
-    navLinks.forEach(link => {
-      link.style.display === 'block' ? link.style.display = 'none' : link.style.display = 'block';
-    });
+    // var navLinks = document.querySelectorAll('.navbar a');
+    // navLinks.forEach(link => {
+    //   link.style.display === 'block' ? link.style.display = 'none' : link.style.display = 'block';
+    // });
     
   }
   
